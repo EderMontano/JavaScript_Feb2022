bc11acd43528bec093c48b50cd26e04d5765483e
EderDev89 <ing.eder.montano@gmail.com>
2022-02-05 10:13:53 -0600

    agregando primera practica

---
+++ PrimerasPracticas/Practica_1.js
@@ -0,0 +1,30 @@
+//Variables y operadores
+var a = 1;
+
+let b = 45;
+
+var c = 6;
+
+var d;
+
+d = a + b;
+
+console.log(d)
+
+console.log("Resultado de la variable d: ",d)
+
+console.log(a+b)
+
+console.log(8+5)
+
+console.log(a, b)
+
+console.log(a-b)
+
+console.log(a*b)
+
+console.log(a/b)
+
+console.log(a%b)
+
+//Diferencia entre var y let, el scope. Var es mas limitado
